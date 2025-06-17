85096a958b013cea066f89e0b62c417525d09586
Nelly <Nelly.sosobrado@gmail.com>
2025-06-17 10:26:31 +0200

    installed nugget packages for first code sql database. Created ApplicationDBcontext

---
+++ ClassLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
+namespace ClassLibrary.Data
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Country> Countries { get; set; } = null!;
+        public virtual DbSet<Customer> Customers { get; set; } = null!;
+    }
+}
